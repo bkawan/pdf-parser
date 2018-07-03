65be3bce8f79da38aa84de593f222aa5c9ea284e
bkawan <bikeshkawang@gmail.com>
2018-07-03 18:16:28 +0545

    add views for documents

--- apps/v1/core/views.py
+++ apps/v1/core/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
 from django.views.generic import TemplateView
 
+from ..document_parser.models import Document
+
 
 class LandingPageView(TemplateView):
     template_name = 'pages/landing_page.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        documents = Document.objects.all()
+        ctx['documents'] = documents
+
+        return ctx

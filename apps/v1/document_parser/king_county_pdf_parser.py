e597e410518be36c4a35eda5c6ee51ff8dd1e8d6
bkawan <bikeshkawang@gmail.com>
2018-07-05 13:37:13 +0545

    clean pdf and extract fields

--- apps/v1/document_parser/king_county_pdf_parser.py
+++ apps/v1/document_parser/king_county_pdf_parser.py
@@ -1,10 +1,50 @@
+import re
+
 from .pdf_parser import PdfParser
 
 
 def clean_king_county_pdf_file(file_path):
-    cleaned_data = ''
-    raw_data = PdfParser(file_path=file_path)
+    parser = PdfParser(file_path=file_path)
+    splitter = "------------------------------------------------------------------------------"
+    raw_data = parser.convert_pdf_to_text()
+    rows = re.split(splitter, raw_data)
+    rows.pop()  ## last one is just page number
+    data = {
+        'case_status_date':'',
+        'case_list':[]
+    }
+    for i, x in enumerate(rows):
+        if i == 0:
+            group = re.search(r'New\s+Case\s+List(.*)PAGE[\s]+\d', x.strip())
+            if group:
+                case_status_data = group.group().split(' ')[3]
+                data['case_status_date'] = case_status_data.strip()
+                each_row = x.strip().replace(group.group(), '').strip().split(" ")
+        else:
+            group = re.search(r'SCOMIS(.*)PAGE[\s]+\d', x.strip())
+            if group:
+                each_row = x.strip().replace(group.group(), '').strip().split(" ")
+            else:
+                each_row = x.strip().split(" ")
 
-    cleaned_data = raw_data.convert_pdf_to_text()
+        try:
+            case_number = each_row[0]
+        except IndexError:
+            case_number = 'Error'
 
-    return cleaned_data
+        try:
+            case_type = each_row[6]
+        except IndexError:
+            case_type = 'Error'
+
+        try:
+            file_date = each_row[3]
+        except IndexError:
+            file_date = 'Error'
+        data['case_list'].append({
+            'case_number':case_number,
+            'case_type':case_type,
+            'file_date':file_date,
+        })
+
+    return data

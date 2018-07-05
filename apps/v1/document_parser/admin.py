bdb08529823cbd42b7ad877f8f6b65c3938ac9b1
bkawan <bikeshkawang@gmail.com>
2018-07-05 12:10:37 +0545

    modify  Document model

--- apps/v1/document_parser/admin.py
+++ apps/v1/document_parser/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Document
 
+
 # Register your models here.
-admin.site.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'file']
+
+
+admin.site.register(Document, DocumentAdmin)
--- apps/v1/document_parser/models.py
+++ apps/v1/document_parser/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
-from ..core.abstract_models import AbstractDocument
+from ..core.abstract_models import AbstractDocument, AbstractSlug
 
 
 # Create your models here.
-class Document(AbstractDocument, models.Model):
+class Document(AbstractSlug, AbstractDocument, models.Model):
     KING_COUNTY = 'KING COUNTY'
     DOCUMENT_FORMAT_TYPE_CHOICES = (
         (KING_COUNTY, KING_COUNTY),
     )
     document_format_type = models.CharField(choices=DOCUMENT_FORMAT_TYPE_CHOICES, default=KING_COUNTY, max_length=255)
+    document_created_at = models.DateTimeField(blank=True, null=True, help_text='Date of  document creation date')
+
+    @property
+    def slug_field(self):
+        return self.file
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.name:
+            self.name = self.file.name
+        super().save(*args, **kwargs)

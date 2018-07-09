0c5514517e5563491ffa1c3573a0726d11dcd971
bkawan <bikeshkawang@gmail.com>
2018-07-09 16:18:42 +0545

    update heroku setings

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,8 +1,15 @@
 DEBUG = False
 import dj_database_url
-from .base import DATABASES
+
+from .base import DATABASES, MIDDLEWARE
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
+
 DATABASES['default'].update(db_from_env)
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+]

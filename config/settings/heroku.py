fe03a662c04a9a7aec067bc5ff891d1540e39a49
bkawan <bikeshkawang@gmail.com>
2018-07-09 16:16:27 +0545

    update heroku settings

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -3,5 +3,6 @@ import dj_database_url
 from .base import DATABASES
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)

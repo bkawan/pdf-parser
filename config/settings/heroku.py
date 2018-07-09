f535d014f170122a2aef0bab0898fca5778d1424
bkawan <bikeshkawang@gmail.com>
2018-07-09 15:59:08 +0545

    add heroku settings

---
+++ config/settings/heroku.py
@@ -0,0 +1,7 @@
+DEBUG = False
+import dj_database_url
+from django.conf.global_settings import DATABASES
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

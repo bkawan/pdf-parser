e923c04431702f2761a55d477afaa5afba84b3fe
bkawan <bikeshkawang@gmail.com>
2018-07-10 12:33:07 +0545

    cahnge admin site header, title, index title

--- config/settings/env.py
+++ config/settings/env.py
@@ -1,3 +1,7 @@
 from .base import *
 from .dev import *
 # from .prod import *
+
+ADMIN_SITE_HEADER = 'PDF Parser Administration'
+ADMIN_SITE_TITLE = 'PDF Parser Administration'
+ADMIN_SITE_INDEX_TITLE = 'Welcome to PDF parser'
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,11 @@ from django.urls import path, include
 
 from .api import urls as api_urls
 
+
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_SITE_INDEX_TITLE
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('document-parser/', include('apps.v1.document_parser.urls')),

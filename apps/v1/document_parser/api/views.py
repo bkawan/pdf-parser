a4e8a74590c20a3244c9a71d8279473e72a8fa01
bkawan <bikeshkawang@gmail.com>
2018-07-22 10:14:49 +0545

    display error from api response to frontend ajax call

--- apps/v1/document_parser/api/views.py
+++ apps/v1/document_parser/api/views.py
@@ -1,3 +1,7 @@
+import os
+
+from django.utils import timezone
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,9 +17,10 @@ class KingCountyPdfParseAPIView(APIView):
         """
         document = get_object_or_404(Document, slug=kwargs.get('slug'))
         document_path = document.file.path
+        if not os.path.isfile(document_path):
+            raise NotFound(f"The request file not found or delete from the server located at {document.file}")
+
         data = clean_king_county_pdf_file(file_path=document_path)
-        data['case_status_date'] = document.document_created_at.strftime('%m/%d/%Y')
+        data['case_status_date'] = timezone.localtime(document.created_at).strftime('%m/%d/%Y')
 
         return Response({'data':data})
-
-
